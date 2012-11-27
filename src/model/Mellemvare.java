d4bba22ef53450c355dd4cee1d057d4cee344f72
JimmyFricke <JimmyFricke@10.97.21.146>
2012-11-27 10:22:37 +0100

    jeg skubber Status

--- src/model/Mellemvare.java
+++ src/model/Mellemvare.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mellemvare {
-	private Status status = Status.TILTØRRING;
+	private Status status = null;
 	private String id;
 	private Produkttype produkttype =null;
 	private List<Toerretid> toerretider = new ArrayList<Toerretid>();
@@ -13,6 +13,7 @@ public class Mellemvare {
 		this.produkttype=produkttype;
 		this.setId(id);
 		createToerretid(tid);
+		status=Status.TILTOERRING;
 	}
 	public Produkttype getProdukttype(){
 		return produkttype;
