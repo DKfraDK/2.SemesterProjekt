38510fcf84872cf2b22052c70ec0fb22aac2d43d
dk <dk@10.97.22.23>
2012-11-28 11:37:24 +0100

    Gui næsten færdig. Mangle lagerstyring funktionalitet

--- src/gui/OpretProdukttypeDialog.java
+++ src/gui/OpretProdukttypeDialog.java
@@ -1,18 +1,27 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import model.Behandling;
+import service.Service;
 
 public class OpretProdukttypeDialog extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
+	private Controller controller = new Controller();
 	private JList produkttype_list;
+	private JTextField navn_txt;
+	private JList behandling_list;
+	private JButton ok_btn, cancel_btn;
 	
 	/**
 	 * Create the dialog.
@@ -23,6 +32,64 @@ public class OpretProdukttypeDialog extends JDialog {
 		setVisible(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
+		
+		JLabel navn_lbl= new JLabel("Navn: ");
+		navn_lbl.setBounds(43, 6, 40, 16);
+		getContentPane().add(navn_lbl);
+		
+		navn_txt = new JTextField();
+		navn_txt.setBounds(95, 0, 134, 28);
+		getContentPane().add(navn_txt);
+		navn_txt.setColumns(10);
+		navn_txt.addActionListener(controller);
+		
+		behandling_list = new JList();
+		behandling_list.setBounds(6, 62, 200, 210);
+		behandling_list.setListData(Service.getBehandling().toArray());
+		getContentPane().add(behandling_list);
+		behandling_list.addListSelectionListener(controller);
+		
+		JLabel lblNewLabel = new JLabel("V\u00E6lg behandling");
+		lblNewLabel.setBounds(29, 34, 177, 16);
+		getContentPane().add(lblNewLabel);
+		
+		ok_btn = new JButton("Ok");
+		ok_btn.setBounds(268, 57, 117, 29);
+		getContentPane().add(ok_btn);
+		ok_btn.addActionListener(controller);
+		
+		cancel_btn = new JButton("Cancel");
+		cancel_btn.setBounds(268, 101, 117, 29);
+		getContentPane().add(cancel_btn);
+		cancel_btn.addActionListener(controller);
+		
 	}
 
+	private class Controller implements ActionListener, ListSelectionListener{
+
+		private Behandling currentSelectedBehandling;
+		
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			if(e.getSource() == behandling_list){
+				currentSelectedBehandling = (Behandling) behandling_list.getSelectedValue();
+			}
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == ok_btn){
+				if(navn_txt.getText().equals("")){
+					JOptionPane.showMessageDialog(null, "Indtast navn", "Fejl", JOptionPane.ERROR_MESSAGE);
+				}else{
+					Service.createProdukttype(navn_txt.getText(), currentSelectedBehandling);
+					produkttype_list.setListData(Service.getProdukttyper().toArray());
+					dispose();
+				}
+			}else if(e.getSource() == cancel_btn){
+				dispose();
+			}	
+		}
+	
+	}
 }
